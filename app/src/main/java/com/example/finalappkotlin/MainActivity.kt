9916d278cba7a53d09314145492f5e299315db8e
diemp94 <33240612+diemp94@users.noreply.github.com>
2019-04-16 21:33:50 -0500

    Initial commit

---
+++ app/src/main/java/com/example/finalappkotlin/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.finalappkotlin
+
+import android.app.ActivityManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var perro = 5
+    }
+}
