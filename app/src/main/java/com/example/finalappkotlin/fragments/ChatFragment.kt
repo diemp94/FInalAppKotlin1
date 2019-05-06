d70ff1d82f14be44bce94608a3839db98dba1e4f
diemp94 <33240612+diemp94@users.noreply.github.com>
2019-05-06 00:35:45 -0500

    Chat implementation
    Se crea la implementacion del chat sin la actualizacion en tiempo real del mismo, unicamente la consulta a FB

--- app/src/main/java/com/example/finalappkotlin/fragments/ChatFragment.kt
+++ app/src/main/java/com/example/finalappkotlin/fragments/ChatFragment.kt
@@ -6,16 +6,93 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.finalappkotlin.R
+import com.example.finalappkotlin.adapters.ChatAdapter
+import com.example.finalappkotlin.models.Message
+import com.example.finalappkotlin.toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.fragment_chat.*
+import kotlinx.android.synthetic.main.fragment_chat.view.*
+import java.util.*
+import kotlin.collections.HashMap
 
 class ChatFragment : Fragment() {
+
+    private lateinit var _view: View
+
+    private val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    private lateinit var currentUser: FirebaseUser
+
+    private val store: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private lateinit var chatDBRef: CollectionReference
+
+    //Recycler
+    private lateinit var adapter: ChatAdapter
+    private val messageList: ArrayList<Message> = ArrayList()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false)
+        _view = inflater.inflate(R.layout.fragment_chat, container, false)
+
+        setUpChatDB()
+        setUpCurrentUser()
+        SetUpRecyclerView()
+        setUpChatBtn()
+
+
+        return _view
+    }
+
+    private fun setUpChatDB() {
+        chatDBRef = store.collection("chat")
+    }
+
+    private fun setUpCurrentUser() {
+        currentUser = mAuth.currentUser!!
+    }
+
+    private fun SetUpRecyclerView() {
+        val layoutManager = LinearLayoutManager(context)
+        adapter = ChatAdapter(messageList, currentUser.uid)
+
+        _view.rvChat.setHasFixedSize(true)
+        _view.rvChat.layoutManager = layoutManager
+        _view.rvChat.itemAnimator = DefaultItemAnimator()
+        _view.rvChat.adapter = adapter
+    }
+
+    private fun setUpChatBtn() {
+        _view.btnSend.setOnClickListener {
+            val messageText = etMessage.text.toString()
+            if(messageText.isNotEmpty()){
+                val message = Message(currentUser.uid,messageText,currentUser.photoUrl.toString(), Date())
+                saveMessage(message)
+                _view.etMessage.setText("")
+            }
+        }
+    }
+
+    private fun saveMessage(message: Message){
+        val newMessage = HashMap <String, Any>()
+        newMessage["authorId"] = message.authorId
+        newMessage["message"] = message.message
+        newMessage["profileImageURL"] = message.profileImageURL
+        newMessage["sentAt"] = message.sentAt
+
+        chatDBRef.add(newMessage)
+                 .addOnCompleteListener{
+                   activity!!.toast("Message added!")
+                }
+                .addOnFailureListener {
+                    activity!!.toast("Message failed try again!")
+                }
     }
 
 
